e07904ee0e51d9dcfaaccd23a72d8aff4fb86380
Asterios <akalogeras@olympia-electronics.gr>
2023-09-13 13:51:19 +0300

    implemented delete action to load from db modal

--- public/components/start-dialog/start-dialog.js
+++ public/components/start-dialog/start-dialog.js
@@ -15,13 +15,14 @@ export default class StartDialog {
     this.enterKeyHandler = [];
     this.inputFocusOutHandler = [];
     this.collectionId = [];
+    this.currentSelection = null;
   }
 
   setComponents(canvas) {
     this._canvas = canvas;
   }
 
-  async render() {
+  render() {
     this.startModal = document.getElementById("start-modal-container");
     this.newCollectionModal = document.getElementById(
       "new-collection-container"
@@ -54,11 +55,13 @@ export default class StartDialog {
       this.loadCollectionsFromDb();
     });
 
-    const response = await fetch("/collections");
-    this.collections = await response.json();
+    this.currentSelection = -1;
   }
 
-  saveCollection() {
+  async saveCollection() {
+    const response = await fetch("/collections");
+    this.collections = await response.json();
+
     const collectionContainer = document.getElementById(
       "new-collection-container"
     );
@@ -154,139 +157,77 @@ export default class StartDialog {
     });
   }
 
-  loadCollectionsFromDb() {
+  async loadCollectionsFromDb() {
+    const response = await fetch("/collections");
+    this.collections = await response.json();
+
+    this.collectionId = [];
+
+    this.collections.forEach(item => {
+      this.collectionId.push(item.Id);
+    });
+
     const collectionContainer = document.getElementById(
       "load-db-collection-container"
     );
     const confirmBtn = document.getElementById("confirm-load-db-modal-btn");
     const cancelBtn = document.getElementById("cancel-load-db-modal-btn");
-    const collections = document.querySelectorAll(`[class*=collection-id]`);
+    const collections = document.querySelectorAll(`[class*="collection-id"]`);
+    const inputContainers = document.querySelectorAll(
+      `[class*="collection-name"]`
+    );
 
     this.buildCollectionList();
 
-    // collections.forEach(item => {
-    //   const collection = item.querySelector("span");
-    //   const input = collection.querySelector("span > input");
-    //   const icons = item.querySelectorAll("i");
-
-    //   const collectionId = this.collections.forEach(element => {
-    //     if (input.value === element.CollectionName) {
-    //       return element.CollectionId;
-    //     }
-    //   });
-
-    //   const collectionHandler = () => {
-    //     if (!input.matches(":focus")) {
-    //       collections.forEach(other => {
-    //         const restInputs = other.querySelector("span > input");
-
-    //         if (restInputs !== input) {
-    //           restInputs.classList.remove("input-selected");
-    //           restInputs.classList.add("input-default");
-    //         }
-    //       });
-
-    //       if (input.classList.contains("input-default")) {
-    //         input.classList.remove("input-default");
-    //         input.classList.add("input-selected");
-    //       } else if (input.classList.contains("input-selected")) {
-    //         input.classList.remove("input-selected");
-    //         input.classList.add("input-default");
-    //       }
-    //     }
-    //   };
-
-    //   const editIconHandler = e => {
-    //     e.stopPropagation();
-
-    //     input.style =
-    //       "color: #635456; text-decoration: underline; pointer-events: unset;";
-    //     input.focus();
-
-    //     input.setSelectionRange(input.value.length, input.value.length);
-    //   };
-
-    //   const delIconHandler = e => {
-    //     e.stopPropagation();
-    //   };
-
-    //   const enterKeyHandler = e => {
-    //     if (e.key === "Enter") {
-    //       input.style = "";
-    //       input.blur();
-
-    //       this.updateName(collectionId, input.value);
-    //     }
-    //   };
-
-    //   const documentClickHandler = e => {
-    //     if (!item.contains(e.target)) {
-    //       input.style = "";
-    //       input.blur();
-
-    //       this.updateName(collectionId, input.value);
-    //     }
-    //   };
-
-    //   this.collectionHandler.push(collectionHandler);
-    //   this.editIconHandler.push(editIconHandler);
-    //   this.delIconHandler.push(delIconHandler);
-    //   this.enterKeyHandler.push(enterKeyHandler);
-    //   this.documentClickHandler.push(documentClickHandler);
-
-    //   collection.addEventListener("click", collectionHandler);
-    //   icons[0].addEventListener("click", editIconHandler);
-    //   icons[1].addEventListener("click", delIconHandler);
-    //   input.addEventListener("keydown", enterKeyHandler);
-    //   document.addEventListener("click", documentClickHandler);
-    // });
+    inputContainers.forEach(item => {
+      item.addEventListener("click", () => {
+        collections.forEach(collection => {
+          let input = collection.querySelector("span > input");
+          if (input.classList.contains("input-selected")) {
+            confirmBtn.disabled = !confirmBtn.disabled;
+          }
+        });
+      });
+    });
 
     confirmBtn.addEventListener("click", async () => {
-      collections.forEach((item, index) => {
-        const collection = item.querySelector("span");
-        const input = collection.querySelector("span > input");
-        const icons = item.querySelectorAll("i");
-
-        collection.removeEventListener("click", this.collectionHandler[index]);
-        icons[0].removeEventListener("click", this.editIconHandler[index]);
-        icons[1].removeEventListener("click", this.delIconHandler[index]);
-        input.removeEventListener("keydown", this.enterKeyHandler[index]);
-        document.removeEventListener("click", this.inputFocusOutHandler[index]);
-      });
-
-      this.collectionHandler = [];
-      this.editIconHandler = [];
-      this.delIconHandler = [];
-      this.enterKeyHandler = [];
-      this.inputFocusOutHandler = [];
+      this.loadDbCollectionModal.classList.add("hidden");
+      this.removeEventListeners(collections);
     });
 
     cancelBtn.addEventListener("click", async () => {
+      const collectionList = document.getElementById("collection-list");
+      collectionList.innerHTML = "";
       collectionContainer.classList.add("hidden");
       this.startModal.classList.remove("hidden");
+      this.removeEventListeners(collections);
     });
   }
 
   buildCollectionList() {
     const collectionList = document.getElementById("collection-list");
+    const confirmBtn = document.getElementById("confirm-load-db-modal-btn");
 
     this.collections.forEach((item, index) => {
       const collectionDiv = document.createElement("div");
-      collectionDiv.classList.add(`mb-2`, `collection-id-${index + 1}`);
+      collectionDiv.classList.add(
+        `mb-2`,
+        `collection-id-${this.collectionId[index]}`
+      );
 
       const span = document.createElement("span");
       span.classList.add(
         `hover:bg-gray-100`,
         `hover:cursor-pointer`,
         `hover:text-header-hover`,
-        `collection-name-${index + 1}`
+        `collection-name-${this.collectionId[index]}`
       );
       span.style.marginRight = "10px";
 
       const input = document.createElement("input");
       input.classList.add(`bg-transparent`, `input-default`);
       input.value = item.CollectionName;
-      input.setAttribute("data-index", index + 1);
+      input.setAttribute("data-index", this.collectionId[index]);
 
       const edit = document.createElement("i");
       edit.classList.add(
@@ -294,7 +235,7 @@ export default class StartDialog {
         `hover:cursor-pointer`,
         `fa`,
         `fa-edit`,
-        `collection-edit-${index + 1}`
+        `collection-edit-${this.collectionId[index]}`
       );
       edit.style.marginRight = "10px";
 
@@ -304,7 +245,7 @@ export default class StartDialog {
         `hover:cursor-pointer`,
         `fa`,
         `fa-trash`,
-        `collection-delete-${index + 1}`
+        `collection-delete-${this.collectionId[index]}`
       );
 
       span.appendChild(input);
@@ -314,55 +255,80 @@ export default class StartDialog {
       collectionList.appendChild(collectionDiv);
 
       const collectionHandler = () => {
+        if (this.currentSelection !== -1 && this.currentSelection !== index) {
+          const previousSelection = document.querySelector(
+            `.collection-id-${this.collectionId[this.currentSelection]} input`
+          );
+
+          previousSelection.classList.remove("input-selected");
+          previousSelection.classList.add("input-default");
+        }
+
         if (!input.matches(":focus")) {
           if (input.classList.contains("input-default")) {
             input.classList.remove("input-default");
             input.classList.add("input-selected");
+            confirmBtn.disabled = false;
           } else if (input.classList.contains("input-selected")) {
             input.classList.remove("input-selected");
             input.classList.add("input-default");
+            confirmBtn.disabled = true;
           }
         }
+
+        this.currentSelection = index;
       };
 
       const editIconHandler = e => {
         e.stopPropagation();
 
+        if (this.currentSelection !== -1 && this.currentSelection !== index) {
+          const previousSelection = document.querySelector(
+            `.collection-id-${this.collectionId[this.currentSelection]} input`
+          );
+
+          previousSelection.classList.remove("input-selected");
+          previousSelection.classList.add("input-default");
+        }
+
         if (edit.classList.contains("fa-edit")) {
           edit.classList.remove("fa-edit");
           edit.classList.add("fa-save");
 
+          input.focus();
           input.style =
             "color: #635456; text-decoration: underline; pointer-events: unset;";
-          input.focus();
 
           input.setSelectionRange(input.value.length, input.value.length);
         } else if (edit.classList.contains("fa-save")) {
           edit.classList.remove("fa-save");
           edit.classList.add("fa-edit");
           input.blur();
+          input.style = "";
 
-          this.updateName(index + 1, input.value);
+          this.updateName(this.collectionId[index], input.value);
         }
       };
 
       const delIconHandler = e => {
         e.stopPropagation();
+
+        this.deleteCollection(this.collectionId[index]);
       };
 
       const enterKeyHandler = e => {
         if (e.key === "Enter") {
-          input.style = "";
-          input.blur();
           edit.classList.remove("fa-save");
           edit.classList.add("fa-edit");
+          input.blur();
+          input.style = "";
 
-          this.updateName(index + 1, input.value);
+          this.updateName(this.collectionId[index], input.value);
         }
       };
 
       const inputFocusOutHandler = e => {
-        if (e.target !== !input && !input.contains(e.target)) {
+        if (!input.contains(e.target)) {
           input.style = "";
           edit.classList.remove("fa-save");
           edit.classList.add("fa-edit");
@@ -375,12 +341,24 @@ export default class StartDialog {
       this.enterKeyHandler.push(enterKeyHandler);
       this.inputFocusOutHandler.push(inputFocusOutHandler);
 
-      collectionDiv.addEventListener("click", collectionHandler);
+      span.addEventListener("click", collectionHandler);
       edit.addEventListener("click", editIconHandler);
       del.addEventListener("click", delIconHandler);
       input.addEventListener("keydown", enterKeyHandler);
-      input.addEventListener("click", inputFocusOutHandler);
+      document.addEventListener("click", inputFocusOutHandler);
     });
+
+    // inputContainers.forEach(item => {
+    //   item.addEventListener("click", () => {
+    //     console.log(`Hi.`);
+    //     collections.forEach(collection => {
+    //       let input = collection.querySelector("span > input");
+    //       if (input.classList.contains("input-selected")) {
+    //         confirmBtn.disabled = false;
+    //       }
+    //     });
+    //   });
+    // });
   }
 
   updateName(collectionId, newCollectionName) {
@@ -433,5 +411,70 @@ export default class StartDialog {
         "A file name can contain only letters, numbers, spaces, and ( ) _ - , . ";
       console.log("Invalid filename. Please enter a valid filename.");
     }
+  }
+
+  deleteCollection(collectionId) {
+    const delModal = document.getElementById("delete-collection-container");
+    const confirmBtn = document.getElementById("delete-collection-confirm-btn");
+    const cancelBtn = document.getElementById("delete-collection-cancel-btn");
+
+    this.loadDbCollectionModal.classList.add("hidden");
+    delModal.classList.remove("hidden");
+
+    confirmBtn.addEventListener("click", () => {
+      fetch(`/delete-collection/${collectionId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(res => {
+          if (!res.ok) {
+            throw new Error(`HTTP error. Status: ${res.status}`);
+          }
+
+          return res.text();
+        })
+        .then(data => {
+          const collectionList = document.getElementById("collection-list");
+          const inputAlert = document.getElementById("load-db-input-alert");
+
+          delModal.classList.add("hidden");
+          this.loadDbCollectionModal.classList.remove("hidden");
+          collectionList.innerHTML = "";
+          inputAlert.innerHTML = "";
+          this.loadCollectionsFromDb();
+
+          console.log(`Collection deleted successfully: ${data}`);
+        })
+        .catch(err => console.error(`Error deleting collection: ${err}`));
+    });
+
+    cancelBtn.addEventListener("click", () => {
+      delModal.classList.add("hidden");
+      this.loadDbCollectionModal.classList.remove("hidden");
+    });
+  }
+
+  removeEventListeners(collections) {
+    collections.forEach((item, index) => {
+      const collection = item.querySelector("span");
+      const input = collection.querySelector("span > input");
+      const icons = item.querySelectorAll("i");
+
+      collection.removeEventListener("click", this.collectionHandler[index]);
+      icons[0].removeEventListener("click", this.editIconHandler[index]);
+      icons[1].removeEventListener("click", this.delIconHandler[index]);
+      input.removeEventListener("keydown", this.enterKeyHandler[index]);
+      document.removeEventListener("click", this.inputFocusOutHandler[index]);
+    });
+
+    this.collectionHandler = [];
+    this.editIconHandler = [];
+    this.delIconHandler = [];
+    this.enterKeyHandler = [];
+    this.inputFocusOutHandler = [];
+
+    this.currentSelection = -1;
   }
 }
