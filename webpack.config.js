f38fd94d3ec82a356b9912e5adaa07e8e9ea45b1
dkiprianou <dkiprianou@olympia-electronics.gr>
2024-11-29 13:59:11 +0200

    tailwind build and classes fixed

--- webpack.config.js
+++ webpack.config.js
@@ -5,29 +5,28 @@ export default {
   entry: "./public/js/index.js",
   output: {
     filename: "bundle.js",
-    path: path.join(path.resolve(), "public/dist")
+    path: path.join(path.resolve(), "public/dist"),
   },
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: "html-loader"
+        use: "html-loader",
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
   },
   resolve: {
-    extensions: [".js", ".css", ".html"]
+    extensions: [".js", ".css", ".html"],
   },
   devServer: {
     static: path.join(path.resolve(), "public"),
     hot: true,
-    open: true
+    open: true,
   },
   plugins: [new CleanWebpackPlugin()],
-  // set to development or production
-  mode: "development"
+  mode: "development", // or "production"
 };
